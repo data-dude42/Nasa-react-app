e4a1041f22810561ef45996f2f447c8d3cd78f64
data-dude42 <ss8820788@gmail.com>
2024-12-28 23:44:21 -0500

    updated components/Main.jsx and index.css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 export default function Main(){
     return (
-        <div>
-            <img src="mars.png" alt="mar-demo-picture"/>
+        <div className="imgContainer">
+            <img src="mars.png" alt="mar-demo-picture" className="bgImage"/>
         </div>
     )
 }
